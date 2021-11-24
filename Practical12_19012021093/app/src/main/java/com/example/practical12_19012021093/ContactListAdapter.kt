8b032032d89973a0e528a96a992a915d43bb9cec
Bhargav Joshi <82135781+bhargav-18@users.noreply.github.com>
2021-11-24 16:50:52 +0530

    lv click left

--- Practical12_19012021093/app/src/main/java/com/example/practical12_19012021093/ContactListAdapter.kt
+++ Practical12_19012021093/app/src/main/java/com/example/practical12_19012021093/ContactListAdapter.kt
@@ -23,13 +23,13 @@ class ContactListAdapter(private var context: Context, var items: ArrayList<Cont
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
-        val view = LayoutInflater.from(context).inflate(R.layout.adapter_layout, parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.row_layout, parent, false)
 
-        val contact_name: TextView = view.findViewById(R.id.name_textview)
-        val contact_num: TextView = view.findViewById(R.id.number_textview)
+        val contactName: TextView = view.findViewById(R.id.tvName)
+        val contactPhone: TextView = view.findViewById(R.id.tvPhone)
 
-        contact_name.text = items[position].name
-        contact_num.text = items[position].num
+        contactName.text = items[position].name
+        contactPhone.text = items[position].num
 
         return view
     }
--- Practical12_19012021093/app/src/main/java/com/example/practical12_19012021093/MainActivity.kt
+++ Practical12_19012021093/app/src/main/java/com/example/practical12_19012021093/MainActivity.kt
@@ -3,68 +3,75 @@ package com.example.practical12_19012021093
 import android.os.AsyncTask
 import android.os.Bundle
 import android.widget.ListView
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import org.json.JSONArray
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
 
-    val listitems = Contact.contactArray as ArrayList<Contact>
-    val adapter = ContactListAdapter(this, listitems)
+    val listItems = Contact.contactArray as ArrayList<Contact>
     lateinit var list: ListView
+    private lateinit var adapter: ContactListAdapter
+    private lateinit var progress: ProgressBar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         list = findViewById(R.id.lv_contacts)
+        adapter = ContactListAdapter(this, listItems)
+
+        progress = findViewById(R.id.progress_bar)
 
         contacts().execute()
     }
 
     inner class contacts() : AsyncTask<String, Void, String>() {
+
         override fun onPreExecute() {
             super.onPreExecute()
-
+            progress.isVisible = true
         }
 
         override fun doInBackground(vararg params: String?): String? {
-            var response: String?
-            try {
-                response =
-                    URL("https://api.json-generator.com/templates/Voyo_eeyQQN4/data?access_token=bu5lpejbtrlwvtx09400gzls8uvl5ljcnwesb4li").readText(
-                        Charsets.UTF_8
-                    )
+            val response: String? = try {
+                URL("https://api.json-generator.com/templates/Voyo_eeyQQN4/data?access_token=bu5lpejbtrlwvtx09400gzls8uvl5ljcnwesb4li").readText(
+                    Charsets.UTF_8
+                )
 
             } catch (e: Exception) {
-                response = null
+                null
             }
             return response
         }
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
+
+            progress.isVisible = false
+
             try {
 
                 /* Extracting JSON returns from the API */
                 val jsonarr = JSONArray(result)
 
-                for (i in 0..7) {
+                for (i in 0 until jsonarr.length()) {
 
-                    val jsonObj_0 = jsonarr.getJSONObject(i)
-                    val name_json = jsonObj_0.getString("name")
-                    val num_json = jsonObj_0.getString("phone")
-                    listitems.add(Contact(name_json, num_json))
-                    list.adapter = adapter
-                    Toast.makeText(applicationContext, name_json, Toast.LENGTH_SHORT).show()
+                    val jsonObj = jsonarr.getJSONObject(i)
+                    val jsonName = jsonObj.getString("name")
+                    val jsonPhone = jsonObj.getString("phone")
+                    listItems.add(Contact(jsonName, jsonPhone))
+
                 }
-
+                list.adapter = adapter
 
             } catch (e: Exception) {
-
-
+                Toast.makeText(applicationContext, e.message.toString(), Toast.LENGTH_SHORT).show()
             }
 
         }
